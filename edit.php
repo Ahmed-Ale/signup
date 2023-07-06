e02c7773bd1ddda5c87779468cdce17f70269d72
Ahmed-Ale <ahmed19aly@gmail.com>
2023-07-06 21:52:50 +0300

    5th commit

--- edit.php
+++ edit.php
@@ -1,43 +1,24 @@
 <?php
     session_start();
     if(!isset($_SESSION["user"])) {
-        header("Location: sign-in.php");
-    } else {
-        require_once("database.php");
-        $username = $_SESSION['user'];
-        $sql = "SELECT * FROM users WHERE username = '$username'";
-        $result = mysqli_query($conn, $sql);
-        $user = mysqli_fetch_assoc($result);
-        echo "<pre>";
-        print_r($user) ;
-        echo "</pre>";
-
+        header("Location: index.php");
     }
-?>
+    require_once "database.php";
+    $currentuser = $_SESSION["user"];
+    $sql = "SELECT * from users WHERE username = '$currentuser'";
+    $result  = mysqli_query($conn,$sql);
+    $user = mysqli_fetch_array($result);
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edits</title>
-</head>
-<body>
-    <div class="container">
-    <form action="edit.php" method="post">
+    if(isset($post["submit"])) {
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $pass_hash = password_hash($password, PASSWORD_DEFAULT);
 
-    
-    <?php
-        if(isset($_POST["submit"])) {
-            $firstname = $_POST["firstname"];
-            $lastname = $_POST["lastname"];
-            $username = $_POST["username"];
-            $email = $_POST["email"];
-            $password = $_POST["password"];
-            $pass_hash = password_hash($password, PASSWORD_DEFAULT);
-            $errors = array();
-
-            if(empty($firstname) || empty($lastname) || empty($username) || empty($email) || empty($password)) {
+        do {
+            if(empty($firstname) || empty($lastname) || empty($username) || empty($email) || empty($password) || empty($repeated_password)) {
                 array_push($errors, "all fields are required.");
             }
 
@@ -49,25 +30,29 @@
                 array_push($errors, "Password must be at least 8 characters.");
             }
 
+            if($password !== $repeated_password) {
+                array_push($errors, "Passwords do not match");
+            }
+
             if(!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/', $password)) {
                 array_push($errors, "password must contain at least one special character
                 , one uppercase letter
                 , one lowercase letter and one number.");
             }
 
-            require_once("database.php");
-            $sql = "SELECT * from users WHERE username = '$username'";
-            $result  = mysqli_query($conn,$sql);
-            $rowcount = mysqli_num_rows($result);
-            if($_POST["username"] !== $user["username"]) {
+            if($user["username"] !== $post["username"]) {
+                $sql = "SELECT * from users WHERE username = '$username'";
+                $result  = mysqli_query($conn,$sql);
+                $rowcount = mysqli_num_rows($result);
                 if($rowcount>0) {
                     array_push($errors,"Username already exists.");
                 }
             }
-            $sql = "SELECT * from users WHERE email = '$email'";
-            $result  = mysqli_query($conn,$sql);
-            $rowcount = mysqli_num_rows($result);
-            if($_POST["email"] !== $user["email"]) {
+
+            if($user["email"] !== $post["email"]) {
+                $sql = "SELECT * from users WHERE email = '$email'";
+                $result  = mysqli_query($conn,$sql);
+                $rowcount = mysqli_num_rows($result);
                 if($rowcount>0) {
                     array_push($errors,"Email already exists.");
                 }
@@ -78,59 +63,64 @@
                     echo "<div class='alert alert-danger'>$error</div>";
                 }
             } else {
-                $id = $user["id"];
-                $sql = "UPDATE 'users' SET ,firstname='$firstname',lastname='$lastname',username='$username',email='$email',password='$pass_hash'
-                WHERE id='$id';";
-                $result = mysqli_query($conn, $sql);
-                if($result) {
-                    if(mysqli_num_rows($result)>0) {
-                        while($row = mysqli_fetch_array($result)) {
-                            $user["firstname"] = "$firstname";
-                            $user["lastname"] = "$lastname";
-                            $user["username"] = "$username";
-                            $user["email"] = "$email";
-                            $user["password"] = "$pass_hash";
-                        }
-                    }
-                    echo "<div class='alert alert-success'>Your data has been updated successfully</div>";
-                }
+                $sql = "UPDATE users ".
+                "SET firstname = '$firstname', lastname = '$lastname', username = '$username', email = '$email', password = '$pass_hash'" . 
+                "WHERE username = '$currentuser'";
+                $result = $conn->query($sql);
             }
-        }
-        ?>
+            echo "data updated successfully";
+        } while (true);
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <form action="edit.php" method="post">
 
+            <input type="hidden" value="<?php echo $id ?>">
+            <br>
+            <div class="form-group">
+                <label for="firstname">First Name</label>
+                <input type="text" id="firstname" name="firstname" placeholder="Enter your first name" value=<?php echo isset($_POST["firstname"]) ? $_POST["firstname"] : $user["firstname"]; ?> >
+            </div>
 
-        <div class="form-group">
-            <label for="firstname">First Name</label>
-            <input type="text" id="firstname" name="firstname" value=<?php echo isset($user["firstname"]) ? $user["firstname"] : ""; ?> >
-        </div>
+            <div class="form-group">
+                <label for="lastname">Last Name</label>
+                <input type="text" id="lastname" name="lastname" placeholder="Enter your last name" value=<?php echo isset($_POST["lastname"]) ? $_POST["lastname"] : $user["lastname"]; ?> >
+            </div>
 
-        <div class="form-group">
-            <label for="lastname">Last Name</label>
-            <input type="text" id="lastname" name="lastname" value=<?php echo isset($user["lastname"]) ? $user["lastname"] : ""; ?> >
-        </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" value=<?php echo isset($_POST["username"]) ? $_POST["username"] : $user["username"]; ?> >
+            </div>
 
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" value=<?php echo isset($user["username"]) ? $user["username"] : ""; ?> >
-        </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" value=<?php echo isset($_POST["email"]) ? $_POST["email"] : $user["email"]; ?> >
+            </div>
 
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email"  value=<?php echo isset($user["email"]) ? $user["email"] : ""; ?> >
-        </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" value=<?php echo isset($_POST["password"]) ? $_POST["password"] : ""; ?> >
+            </div>
 
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="New Password" value=<?php echo isset($_POST["password"]) ? $_POST["password"] : ""; ?> >
-        </div>
+            <div class="form-group">
+                <label for="repeated_password">Repeat Password</label>
+                <input type="password" id="repeated_password" name="repeated_password" placeholder="Repeat your password" value=<?php echo isset($_POST["repeated_password"]) ? $_POST["repeated_password"] : ""; ?> >
+            </div>
 
-
-        <div class="form-btn">
-            <input type="submit" name="submit" value="Edit">
-        </div>
+            <div class="form-btn">
+                <input type="submit" name="submit" value="Update">
+            </div>
             <p><a href="profile.php">Back to profile</a></p>
-        
-    </form>
+        </form>
     </div>
 </body>
 </html>
